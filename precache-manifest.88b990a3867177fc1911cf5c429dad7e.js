3335c1f13a3d95b2bf7d138112d7b9775b41465b
Ryan Woo <devrwoo@gmail.com>
2019-04-02 16:08:23 -0700

    Updates

---
+++ precache-manifest.88b990a3867177fc1911cf5c429dad7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c55df0e413b7b82846d8",
+    "url": "/movie-app/static/css/main.21c30976.chunk.css"
+  },
+  {
+    "revision": "c55df0e413b7b82846d8",
+    "url": "/movie-app/static/js/main.022ff171.chunk.js"
+  },
+  {
+    "revision": "6e3d07953dfb554cfb69",
+    "url": "/movie-app/static/js/runtime~main.927c110c.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie-app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "6cadf8015cd9d37d6effbc1b98de0885",
+    "url": "/movie-app/index.html"
+  }
+];
